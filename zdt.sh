c831bc16d35a346def383ed19d1d22e52829aee4
muhammad1505 <mylearning1505@gmail.com>
2026-06-16 19:29:11 +0800

    Release: Version 3.4.4

--- zdt.sh
+++ zdt.sh
@@ -35,7 +35,7 @@ set -uo pipefail
 # ==========================================
 # CONSTANTS
 # ==========================================
-readonly APP_VERSION="3.4.3"
+readonly APP_VERSION="3.4.4"
 readonly APP_NAME="Zaki Downloader Tools"
 readonly ZDT_VENV_DIR="$HOME/.local/share/zdt/venv"
 readonly ZDT_CONFIG_FILE="$HOME/.config/zdt/config.env"
