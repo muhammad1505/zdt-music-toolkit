d4b6f020e973ef116e253299b4930ffb958f70fb
muhammad1505 <mylearning1505@gmail.com>
2026-06-17 18:39:10 +0800

    Bump version to 3.8.2

--- zdt.sh
+++ zdt.sh
@@ -2,9 +2,9 @@
 export LC_ALL=C.UTF-8
 #
 # zdt.sh — Universal Music Toolkit (Modular Build)
-# Version : 3.8.1
+# Version : 3.8.2
 set -uo pipefail
-readonly APP_VERSION="3.8.1"
+readonly APP_VERSION="3.8.2"
 
 SCRIPT_PATH="$(cd "$(dirname "$0")" && pwd)/$(basename "$0")"
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
