032f7b6396f198c7edaf414898bbbf000db1af65
muhammad1505 <mylearning1505@gmail.com>
2026-06-23 15:05:18 +0800

    fix: Remove __pycache__ from Git tracking

--- zdt.sh
+++ zdt.sh
@@ -2,9 +2,9 @@
 export LC_ALL=C.UTF-8
 #
 # zdt.sh — Universal Music Toolkit (Modular Build)
-# Version : 4.1.74
+# Version : 4.1.75
 set -uo pipefail
-readonly APP_VERSION="4.1.74"
+readonly APP_VERSION="4.1.75"
 export ZDT_VERSION="$APP_VERSION"
 
 SCRIPT_PATH="$(cd "$(dirname "$0")" && pwd)/$(basename "$0")"
